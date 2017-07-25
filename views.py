89e47031d37db07de41029a282b73b6d19cc7a43
Matthew Krohn <makrohn@gmail.com>
2017-07-25 19:51:38 -0400

    Trying to pull parameters into result.

--- views.py
+++ views.py
@@ -5,13 +5,24 @@ from rpcg_web import character
 import json
 import collections
 
-def index(request):
-    return HttpResponse("Hello World")
-
-def create_npc(request):
-    new_npc = character.NPC("Arik", "Bard", "Dwarf", 4, "Hill")
+def create_npc(request, classname, race, subrace):
+    new_npc = character.NPC("Arik", classname, race, 4, subrace)
     npc_sheet = new_npc.sheet
     template = loader.get_template('create_npc.html')
     npc_sheet["spell_level_counter"] = 1
     return HttpResponse(template.render(npc_sheet, request))
     # return HttpResponse(npc_json)
+
+def index(request):
+    subrace_list = {}
+    for race in character.races.RACE_LIST:
+        race_def = character.races.load_race_file(race)
+        if "Subraces" in race_def:
+            subrace_list[race] = race_def["Subraces"]
+    context = {
+        "Races": character.races.RACE_LIST,
+        "Classes": character.classes.CLASS_LIST,
+        "Subrace_List": subrace_list,
+        }
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
